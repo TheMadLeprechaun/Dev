f5287209ee46318b60682687e2d8c1253ac427eb
Jordan <jahill1993@gmail.com>
2018-05-17 10:02:29 -0500

    Code change
    Added comments and changed code in count_numbers.cbl

--- Programs/COBOL/count_numbers.cbl
+++ Programs/COBOL/count_numbers.cbl
@@ -10,23 +10,27 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
 
+      *Variable Declarations
        01 WS-NUMBER PIC 9(10).
        01 WS-COUNTER PIC 9(10) VALUE 0.
 
        PROCEDURE DIVISION.
 
-       PERFORM GET-INPUT WITH TEST AFTER UNTIL WS-NUMBER>0 OR
-       WS-NUMBER<0.
+      *Run the 'GET-INPUT' paragraph until WS-NUMBER is greater than 0
+       PERFORM GET-INPUT WITH TEST AFTER UNTIL WS-NUMBER>0.
 
+      *Run the 'COUNT-NUMBERS' paragraph until WS-COUNTER is equal to WS-NUMBER
        PERFORM COUNT-NUMBERS UNTIL WS-COUNTER=WS-NUMBER.
 
        GOBACK.
 
+      *Get input from the user and store in WS-NUMBER
        GET-INPUT.
 
            DISPLAY "Enter a number greater than zero: "
            ACCEPT WS-NUMBER.
 
+      *Add 1 to WS-COUNTER amd display the results
        COUNT-NUMBERS.
 
            ADD 1 TO WS-COUNTER.
